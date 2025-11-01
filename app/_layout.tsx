a2b990094de3cb1b2fcdf4731ecc6f92f607bf80
H82M19\Student <Student>
2025-11-01 16:15:58 +0700

    chore(repo): bootstrap Expo project and base files

---
+++ app/_layout.tsx
@@ -0,0 +1,22 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import 'react-native-reanimated';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import { useColorScheme } from '@/hooks/use-color-scheme';
+import HomeScreen from './(tabs)';
+
+export const unstable_settings = {
+  anchor: '(tabs)',
+};
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <SafeAreaProvider>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <HomeScreen />
+      </ThemeProvider>
+    </SafeAreaProvider>
+  );
+}
